e006a3ddc73cba66ed7d796a0ac5032c1301da8c
Junior <adilson.juniorcomunicacao@gmail.com>
2023-01-08 01:47:25 -0300

    atualizacao

--- projeto node/main.js
+++ projeto node/main.js
@@ -1,2 +1,19 @@
-import Express  from "express";
+import Express, { response }  from "express";
 
+// instancia sempre me retorna o produto final produto final 
+
+const app = Express()
+
+// CRUD = CREATE, READ, UPDATE, DELETE 
+
+// read = GET / caminho "/" (main/index) rotal principal
+app.get("/", (request, response) => {
+    response.send("ola")
+})
+
+
+// ouvindo de porta 
+const port = 3000 
+app .listen(port, () => {
+    console.log("Api online na porta: http://localhost:3000")
+})
