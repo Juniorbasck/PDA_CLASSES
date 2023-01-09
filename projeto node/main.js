eb74e2bffd99b0f613295efe7f8b886e1f509e7a
Junior <adilson.juniorcomunicacao@gmail.com>
2023-01-09 13:38:32 -0300

    atualizacoes

--- projeto node/main.js
+++ projeto node/main.js
@@ -3,13 +3,31 @@ import Express, { response }  from "express";
 // instancia sempre me retorna o produto final produto final 
 
 const app = Express()
-
+// CRUD = index
 // CRUD = CREATE, READ, UPDATE, DELETE 
 
-// read = GET / caminho "/" (main/index) rotal principal
+// read = GET / caminho "/" (main/index) rota principal
 app.get("/", (request, response) => {
     response.send("ola")
 })
+
+// create = POST 
+app.post("/", (request, response) => {
+
+    response.send("Usuario criado")
+
+})
+
+// Update = PUT
+app.put("/", (request, response) => {
+    response.send("Usuario atulizado")
+})
+
+// Delete = delete 
+app.delete("/", (request, response) => {
+    response.send("usuario deletado")   
+})
+
 
 
 // ouvindo de porta 
