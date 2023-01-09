5c70f2607f8c51549bde6715aa1f86f646e59f25
Junior <adilson.juniorcomunicacao@gmail.com>
2023-01-09 15:08:30 -0300

    atualizacoes

--- projeto node/main.js
+++ projeto node/main.js
@@ -1,4 +1,4 @@
-import Express, { response }  from "express";
+import Express, { request, response }  from "express";
 
 // instancia sempre me retorna o produto final produto final 
 
@@ -7,23 +7,36 @@ const app = Express()
 //middlewares = meio de compo 
 app.use(Express.json()) 
 
+// Gargalho - para quando está acontecendo muitas resqusições
+app.use((request, response, next) =>{
+    next()
+})
+
+//simular dados 
+const bd_usarios = []
+
+
 // CRUD = index
 // CRUD = CREATE, READ, UPDATE, DELETE 
 
 // read = GET / caminho "/" (main/index) rota principal
 app.get("/", (request, response) => {
-    response.send("ola")
+    response.json(bd_usarios)
 })
 
 // create = POST 
 app.post("/", (request, response) => {
 
+    bd_usarios.push(request.body)
     response.send(`usuario ${request.body.nome} foi criado com sucesso`)
 
 })
 
 // Update = PUT
 app.put("/", (request, response) => {
+    bd_usarios.map((elemento, index) => {
+        console.log(elemento, index)
+    })
     response.send("Usuario atulizado")
 })
 
