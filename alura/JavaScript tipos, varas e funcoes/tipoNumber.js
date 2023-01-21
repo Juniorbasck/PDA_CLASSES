0c9e0cad3c773f15251cc8893937c7e37a721c67
Junior <adilson.juniorcomunicacao@gmail.com>
2023-01-21 14:18:01 -0300

    atualizacoes

---
+++ alura/JavaScript tipos, varas e funcoes/tipoNumber.js
@@ -0,0 +1,12 @@
+// tipo number
+ 
+const meuNumero = 3;
+
+const primeiroNumero = 1; 
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero; 
+
+console.log(operacaoMatematica);
+
+// NaN -> not a number (não é um numero)
