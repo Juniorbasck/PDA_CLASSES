284784b4564781438c8be5b89a4008e541e4c4ec
Junior <adilson.juniorcomunicacao@gmail.com>
2023-01-22 20:03:08 -0300

    atualizacoes

---
+++ alura/JavaScript tipos, varas e funcoes/tipoString.js
@@ -0,0 +1,21 @@
+const texto1 = "ola mundo";
+
+const texto2 = 'ola mundo!';
+const senha = "senhasupersegura12"
+
+//O java script para entender uma cadeia de letras precisa ser como string, o mesmo serve para letras
+
+const StringDeNumeros = "223982"; //isso para o javaScript ainda é uma string
+
+
+const citacao = "Meu nomes é: ";
+const meuNome = "junior";
+
+console.log(citacao + meuNome);
+
+//TEMPLATE STRING OU TEMPLATE LITERAL
+
+// concatenacao (+)
+
+
+
