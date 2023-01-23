aa90448546124d4790f95ebb0154e8efd9d690d2
Junior <adilson.juniorcomunicacao@gmail.com>
2023-01-23 19:46:47 -0300

    atualizacoes

---
+++ alura/JavaScript tipos, varas e funcoes/conversoes.js
@@ -0,0 +1,15 @@
+//tipo de dados
+//booleanos
+
+
+//conversão imprecita 
+const numero = 456;
+const numeroString = "456";
+
+
+//number()
+//String()
+
+console.log(numero +  Number(numeroString));
+
+//conversão explicita
