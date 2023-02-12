27529cc697fc245dd2ab913e2ecf56c0bb2fa05d
Junior <adilson.juniorcomunicacao@gmail.com>
2023-02-12 18:48:14 -0300

    atualizacoes

---
+++ alura/Curso javaScript Arrays/jutandoArrays.js
@@ -0,0 +1,7 @@
+const salaDepython = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'guilherme']; 
+
+const salaJavascript =['Aline', 'Fabiana', 'Andre', 'Carlos', 'Paulo'];
+
+const unificada = salaDepython.concat(salaJavascript);
+
+console.log(unificada); 
---
+++ alura/Curso javaScript Arrays/listaDuasdimencoes.js
@@ -0,0 +1,7 @@
+const sala = ['Bia', 'Viviana', 'Isabela', 'Vínicius'];
+
+const medias = [10, 7, 9, 6]
+
+let listaDenotas = [sala, medias];
+
+console.log(`${listaDenotas[0][0]}, a sua media é ${listaDenotas[1][0]}`);
