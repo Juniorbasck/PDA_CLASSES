8bf975151fe8df16c23cc03278dda1d09fedbfd9
Junior <adilson.juniorcomunicacao@gmail.com>
2023-02-15 22:56:44 -0300

    atualizacoes

---
+++ alura/React desenvolvendo com javaScript/organo/src/componetes/compoTexto/index.js
@@ -0,0 +1,12 @@
+import './campoTexto.css'
+
+const CampoTexto = () => {
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input placeholder='DIgite seu nome' />
+        </div> 
+    )
+}
+
+export default CampoTexto; 
