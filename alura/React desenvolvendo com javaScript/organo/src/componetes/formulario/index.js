5268be65e51c502cc25e4af2542816ceda67685f
Junior <adilson.juniorcomunicacao@gmail.com>
2023-02-17 17:16:06 -0300

    atualizacoes

--- alura/React desenvolvendo com javaScript/organo/src/componetes/formulario/index.js
+++ alura/React desenvolvendo com javaScript/organo/src/componetes/formulario/index.js
@@ -1,14 +1,27 @@
+import ListaSuspensa from '../ListaSuspensa';
 import CampoTexto from '../compoTexto';
 import './formulario.css';
 
 const Formulario = () => {
+    
+    const times = [
+        'Programação',
+        'FrontEnd',
+        'Data Sciencia',
+        'Devops',
+        'Design', 
+        'Mobile',
+        'Inovação e gestão'
+    ]
+
     return (
         <selection className="container">
             <form>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto label="Nome" placeholder="Digite seu nome"/>
                 <CampoTexto label="cargo" placeholder="Digite seu caro"/>
-                <CampoTexto label="imagem" placeholder="Digite o enderço da imagem"/> 
+                <CampoTexto label="imagem" placeholder="Digite o enderço da imagem"/>
+                <ListaSuspensa label="Time" itens={times}/>
             </form>
         </selection>
     )
