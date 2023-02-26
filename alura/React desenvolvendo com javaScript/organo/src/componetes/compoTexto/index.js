34470c93a100703bd39d5e02ac9f4da68b4eaae7
Junior <adilson.juniorcomunicacao@gmail.com>
2023-02-26 18:47:59 -0300

    atualizacoes

--- alura/React desenvolvendo com javaScript/organo/src/componetes/compoTexto/index.js
+++ alura/React desenvolvendo com javaScript/organo/src/componetes/compoTexto/index.js
@@ -2,10 +2,17 @@ import './campoTexto.css'
 
 const CampoTexto = (props) => {
     
+    let valor = 'guilherme silveira'
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value;
+        console.log(valor)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div> 
     )
 }
