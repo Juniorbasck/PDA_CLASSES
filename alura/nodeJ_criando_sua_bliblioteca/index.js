ee3b3ad09a072ec52bb99c96033246446e18cd0e
Junior <adilson.juniorcomunicacao@gmail.com>
2023-03-14 21:49:09 -0300

    atualizacoes

--- alura/nodeJ_criando_sua_bliblioteca/index.js
+++ alura/nodeJ_criando_sua_bliblioteca/index.js
@@ -1,7 +1,24 @@
+import fs from 'fs';
 import chalk from "chalk";
 
-console.log(chalk.blue('Ola mundo'));
 
-console.log('olá mundo');
-console.log('ão geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a p');
-console.log('olá mundo');
+function trataErro(erro){
+
+    throw new Error(erro);
+}
+
+function pegarArquivo(caminhoArquivo){
+    
+    const encoding = 'utf-8';
+    fs.readFile(caminhoArquivo, encoding, (erro, retorno) => {
+
+        if(erro){
+            trataErro(erro);
+        }
+
+        console.log(chalk.green(retorno));
+    })
+
+}
+
+pegarArquivo('./arquivos/texto.md');
