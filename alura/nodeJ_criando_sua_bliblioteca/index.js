36c19d3944e4bf826c8618bf695a3e8b16ecfe97
Junior <94143121+Juniorbasck@users.noreply.github.com>
2023-04-21 16:33:18 +0100

    arrumando bugs

--- alura/nodeJ_criando_sua_bliblioteca/index.js
+++ alura/nodeJ_criando_sua_bliblioteca/index.js
@@ -1,22 +1,30 @@
 import fs from 'fs';
 import chalk from "chalk";
 
-const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).';
 
 function extraiLinks(texto){
     
-    const regex = /\[([^\]]*)\]\(https?:\/\/[^$#\s].[^\s]*\)/gm;
-    const linksExtraidos = texto.match(regex);
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
 
-    console.log(linksExtraidos);
+    const ArrayResultados = [];
+    let temp
+    
+    while((temp = regex.exec(texto)) !== null){
+
+        ArrayResultados.push({ [temp[1]]: temp[2] });
+
+    }
+    
+    return (ArrayResultados);   
 }
 
-extraiLinks9(texto);
+
+
 
 function trataErro(erro){
     console.log(erro)
-    throw new Error(chalk.red(erro.code, 'Não é arquivo no diretorio PORRAAA'));
-}
+    throw new Error(chalk.red(erro.code, '  Não é arquivo no diretorio PORRAAA'));
+}   
 
 //async e await
 
@@ -35,14 +43,14 @@ function trataErro(erro){
     
 }
 
-// function pegarArquivo(caminhoArquivo){
+// // function pegarArquivo(caminhoArquivo){
 
-//      const encoding = 'utf-8';
-//      fs.promises.readFile(caminhoArquivo, encoding)
-//      .then((texo) => chalk.green(console.log(texo)))
-//      .catch((erro) => trataErro(erro))
+// //      const encoding = 'utf-8';
+// //      fs.promises.readFile(caminhoArquivo, encoding)
+// //      .then((texo) => chalk.green(console.log(texo)))
+// //      .catch((erro) => trataErro(erro))
 
-// }  
+// // }  
 
 
 /* function pegarArquivo(caminhoArquivo){
@@ -60,4 +68,4 @@ function trataErro(erro){
 } */
 
 
-//pegarArquivo('./arquivos/texto.md');
+pegarArquivo('./arquivos/texto.md');
